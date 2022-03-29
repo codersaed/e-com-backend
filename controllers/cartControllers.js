3aca24a4bd59dca380dab8d0c6100d090062b874
codersaed <codersaed@gmail.com>
2022-03-29 21:54:40 +0600

    update

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -1,24 +1,7 @@
 const _ = require("lodash");
 const { CartItem } = require("../models/cartItem");
 
-module.exports.createCartItem = async (req, res) => {
-  let { price, product } = _.pick(req.body, ["price", "product"]);
-  const item = await CartItem.findOne({
-    user: req.user._id,
-    product: product,
-  });
-  if (item) return res.status(400).send("Item already exists in Cart!");
-  let cartItem = new CartItem({
-    price: price,
-    product: product,
-    user: req.user._id,
-  });
-  const result = await cartItem.save();
-  res.status(201).send({
-    message: "Added to cart successfully!",
-    data: result,
-  });
-};
+
 
 module.exports.getCartItem = async (req, res) => {
   const cartItems = await CartItem.find({
